998660217fa29ee8eebd51bb26d919390956f5a4
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-07-26 18:22:52 +0000

    Example of fibonacci

---
+++ fibonacci.java
@@ -0,0 +1,20 @@
+public class fibonacci{
+    public static int fib(int n){
+        if(n==0||n==1){
+            return n;
+        }
+        if(n==2){
+            return n-1;
+
+        }
+        int fnm1=fib(n-1);
+        int fnm2=fib(n-2);
+        int fn=fnm1+fnm2;
+        return fn;
+
+    }
+    public static void main(String args[]){
+        int n=25;
+        System.out.println(fib(n));
+    }
+}
