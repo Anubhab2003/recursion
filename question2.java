8b00ab283ad4f979a38f0a9b99182c26a605cb8c
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-08-07 15:04:12 +0000

    Important

---
+++ question2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class question2{
+
+    static String digits[]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    public static void eng(int n){
+        if(n==0){
+            return;
+        }
+        int lastdigit=n%10;
+        eng(n/10);
+        System.out.print(digits[lastdigit]+" ");
+    }
+        
+    public static void main(String args[]){
+        eng(1947);
+    }
+}
